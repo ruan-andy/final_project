dc279456ea2806b1d6a076d521a141fcbb759a63
Demo User <demouser@MacBook-Air-8.attlocal.net>
2016-07-25 22:52:54 -0700

    got comment section showing, but no comments displaying #struggle

--- bounce/main.py
+++ bounce/main.py
@@ -8,18 +8,12 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
 
 class Idea(ndb.Model):
-    title = ndb.StringProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
     text = ndb.StringProperty()
-    reference = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    name = ndb.StringProperty()
+
     def url(self):
         return '/idea?key=' + self.key.urlsafe()
-
-class Comment(ndb.Model):
-    name = ndb.StringProperty()
-    text = ndb.StringProperty()
-    date = ndb.DateTimeProperty(auto_now_add=True)
-    #post_key = ndb.KeyProperty(kind = Post)
 
 class HomeHandler(webapp2.RequestHandler):
     def get(self):
@@ -28,8 +22,18 @@ class HomeHandler(webapp2.RequestHandler):
 
 class IdeaHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('create.html')
+        template = jinja_environment.get_template('idea.html')
         self.response.write(template.render())
+
+    def post(self):
+        # get request
+        name = self.request.get('name')
+        text = self.request.get('text')
+        # logic
+        idea = Idea(name=name, text=text)
+        idea.put()
+        # render
+        self.redirect('/idea')
 
 
 class CreateHandler(webapp2.RequestHandler):
@@ -52,10 +56,10 @@ class CreateHandler(webapp2.RequestHandler):
 
         ideas = Idea.query().fetch()
         template_values = {'ideas':ideas}
-        template = jinja_environment.get_template('idea.html')
+        template = jinja_environment.get_template('createidea.html')
         self.response.write(template.render(template_values))
 
-        self.redirect('/idea')
+        self.redirect('/create')
 
 app = webapp2.WSGIApplication([
     ('/', HomeHandler),
