e63a314767faa284eac3bdf3160bb808d1092309
Demo User <demouser@MacBook-Air-8.local>
2016-07-28 16:16:09 -0700

    hold on now

--- bounce/main.py
+++ bounce/main.py
@@ -195,8 +195,8 @@ class UpdateHandler(webapp2.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/ulist', UserHandler),
-    ('/signin', HomeHandler),
-    ('/', SignInHandler),
+    ('/signin', SignInHandler),
+    ('/', HomeHandler),
     ('/create', CreateHandler),
     ('/idea', IdeaHandler),
     ('/list', ListHandler),
